087a082962514a092728d4563f98e6e6b70e0e84
stasserebrennikov59@gmail.com <stasserebrennikov59@gmail.com>
2022-01-28 01:22:51 +0300

    Добавьте файлы проекта.

---
+++ Lab17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Score<int> score1 = new Score<int>();
+            score1.Input();
+            Console.WriteLine(score1.GetInfo());
+
+            Score<string> score2 = new Score<string>();
+            score2.Input();
+            Console.WriteLine(score2.GetInfo());
+            Console.ReadKey();  
+        }
+
+    }
+}
---
+++ Lab17/Score.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Score<T>
+    {
+        public T Code { get; set; }
+        public double Balance { get; set; }
+        public string Name { get; set; }
+
+        public string GetInfo()
+        {
+            return$"{Code} {Balance} {Name}";
+        }
+        public void Input()
+        {
+            Console.WriteLine("Введите счет");
+            string stringCode = Console.ReadLine(); 
+            Code = (T)Convert.ChangeType(stringCode,typeof(T));
+
+            Console.WriteLine("Введите баланс  банковского счета");
+            Balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите ФИО");
+            Name = Console.ReadLine();  
+
+
+
+
+        }
+    }
+}
